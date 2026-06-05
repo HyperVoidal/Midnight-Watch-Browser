182a0437e17753545e85116bb07dcac5cc164fd8
HyperVoidal <samuel.magri@education.nsw.gov.au>
2026-06-05 12:44:18 +1000

    Removed C++ backend
    This incarnation of the Midnight Watch browser brings changes to the C++ backend.
    
    Testing of internal systems during working with autopytoexe has revealed that quite a few commits back I actually removed the C++ backend important components and simply didn't need it, but was verifying the files anyway. This likely occurred during updates to the adblocker engine and I temporarily disconnected the backend interpreter. However, considering the massive headache C++ work has been with file building and the completely lacking importance of the program since the current adblocker incarnation is more than efficient and simply doesn't need the C++ additional system, I have chosen to simply remove the files. It is my hope that changing this backend structure will make future developments and exe file exports more approachable.

--- Main_Repo/src/engine_bridge.py
+++ Main_Repo/src/engine_bridge.py
@@ -6,32 +6,6 @@ from pathlib import Path
 from PySide6.QtWebEngineCore import QWebEngineUrlRequestInfo
 srcSourceDir = Path(__file__).parent
 
-def get_library():
-    #Locate .so file for processing in the build path
-    base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    lib_path = os.path.join(base_path, "build", "libMidnightCPP.so")
-    
-    if not os.path.exists(lib_path):
-        raise FileNotFoundError(f"C++ Library not found at {lib_path}. Did you run 'make'?")
-
-    #Load the library
-    midnight_lib = ctypes.CDLL(lib_path)
-
-    #Define the argument and return types for verify_url_safe
-    #This is crucial so Python doesn't crash the C++ side
-    midnight_lib.verify_url_safe.argtypes = [ctypes.c_char_p]
-    midnight_lib.verify_url_safe.restype = ctypes.c_int
-    
-    return midnight_lib
-
-#Initialise the library
-try:
-    midnight_core = get_library()
-except Exception as e:
-    print(f"Error loading C++ core: {e}")
-    midnight_core = None
-
-
 def load_engine():
     try:
         path = srcSourceDir / "data" / "urlblockerlist.txt"
